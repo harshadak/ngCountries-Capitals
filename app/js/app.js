f0d7999f16dfbfe784685597de792a31150714e6
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2016-03-14 11:59:11 -0700

    Added the script files. Created the functioning of the app. Continuing styling the app.

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,17 @@
-angular.module('myApp', ['ngRoute'])
+angular.module('myApp', ['APIServices', 'DataServices', 'ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
-
-    }])
+        $routeProvider.when('/home', {
+                templateUrl: './home.html'
+            })
+            .when('/countries', {
+                templateUrl: './country-list.html',
+                controller: 'countryListCtrl'
+            })
+            .when('/countries/:countryName', {
+                templateUrl: './country-details.html',
+                controller: 'countryDetailsCtrl'
+            })
+            .otherwise('/home', {
+                templateUrl: './home.html',
+            });
+    }]);
---
+++ app/js/controllers.js
@@ -0,0 +1,52 @@
+angular.module('myApp')
+    .controller('countryListCtrl', ['$scope', '$location', '$filter', 'DataFactory', '$q', function ($scope, $location, $filter, DataFactory, $q) {
+        'use strict';
+
+        var toString = Object.prototype.toString;
+
+        //checking to see if API call returned any data
+        $q.when(DataFactory.countries).then(function (result) {
+            //checking to see if promise returns an object result
+            if (toString.call(DataFactory.countries) == '[object Object]') {
+                DataFactory.countries = result.geonames;
+            }
+            $scope.countries = DataFactory.countries;
+        });
+
+        $scope.showDetail = function (country) {
+            $location.path('/countries/' + country.countryName);
+        };
+
+
+        $scope.predicate = 'countryName';
+        $scope.reverse = false;
+
+        angular.forEach($scope.countries, function (country) {
+            country.areaInSqKm = parseFloat(country.areaInSqKm);
+            country.population = parseFloat(country.population);
+        });
+
+
+        $scope.startsWith = function (actual, expected) {
+            var lowerStr = (actual + "").toLowerCase();
+            return lowerStr.indexOf(expected.toLowerCase()) === 0;
+        };
+    }])
+    .controller('countryDetailsCtrl', ['$scope', '$route', 'DataFactory', function ($scope, $route, DataFactory) {
+        DataFactory.getCountry($route.current.params.countryCode).then(function (result) {
+            $scope.country = result;
+            console.log($scope.country);
+        });
+
+        DataFactory.getCapitals($route.current.params.countryCode).then(function (result) {
+            $scope.capital = result;
+            $scope.capitalPopulation = $scope.capital.population;
+        });
+
+        DataFactory.getNeighbors($route.current.params.countryCode).then(function (result) {
+            $scope.neighbors = result.geonames;
+        });
+
+        $scope.flag = $route.current.params.countryCode.toLowerCase();
+        $scope.map = $route.current.params.countryCode;
+    }]);
